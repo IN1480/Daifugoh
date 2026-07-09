5c689742ed306c5fd3f2cbe7436b170f0173dd76
IN1480 <niko.ishiyama@icloud.com>
2026-07-09 16:31:55 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const title = document.getElementById("title-screen");
-const game = document.getElementById("game-screen");
-
-document.getElementById("start-button").addEventListener("pointerup", () => {
-    title.classList.add("hidden");
-    game.classList.remove("hidden");
-});
