dd777bceb0e5f60c9775821902797818dcc026a6
IN1480 <niko.ishiyama@icloud.com>
2026-07-12 16:44:25 +0900

    Add files via upload

--- js/ui/screen/titleScreen.js
+++ js/ui/screen/titleScreen.js
@@ -9,10 +9,26 @@ export function initTitle(){
     const rule = document.getElementById("rule-screen");
 
     const input = document.getElementById("name-input");
-    const startButton = document.getElementById("start-button");
+    const soloButton = document.getElementById("start-solo-button");
+    const multiButton = document.getElementById("start-multi-button");
     const ruleButton = document.getElementById("rule-button");
 
-    startButton.addEventListener("pointerup", () => {
+    soloButton.addEventListener("pointerup", () => {
+        // 名前取得
+        const name = input.value.trim() || "プレイヤー";
+        localStorage.setItem("playerName", name);
+
+        // プレイヤー作成
+        const player = new Player(name);
+
+        // 画面切り替え
+        screenManager(title, game);
+
+        // ゲーム開始
+        startGame(player);
+    });
+
+    multiButton.addEventListener("pointerup", () => {
         // 名前取得
         const name = input.value.trim() || "プレイヤー";
         localStorage.setItem("playerName", name);
